8d29863d8c95d02d3eaebd4eb72ed85f7732acc6
tingfeng-key <1770963469@qq.com>
2020-04-27 16:17:53 +0800

    优化配置文件

--- config/comment.php
+++ config/comment.php
@@ -2,7 +2,7 @@
 return [
     'domains' => env("COMMENT_DOMAINS", "127.0.0.1|localhost"),
     'site_name' => env("COMMENT_SITE_NAME", "收藏屋"),
-    'site_count_url' => 'https://new.cnzz.com/v1/login.php?siteid=1278840518',
+    'site_count_url' => env("SITE_COUNT_URL", 'https://new.cnzz.com/v1/login.php?siteid=1278840518'),
     'enable_page_share' => env("COMMENT_ENABLE_PAGE_SHARE", false),
     'enable_page_analytics' => env("COMMENT_ENABLE_PAGE_ANALYTICS", false)
 ];
