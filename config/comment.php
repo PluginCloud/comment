520e0443d9f22ec8161b9bb85df12284138cfedc
tingfeng-key <1770963469@qq.com>
2020-04-27 16:19:17 +0800

    优化配置文件

--- config/comment.php
+++ config/comment.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'domains' => env("COMMENT_DOMAINS", "127.0.0.1|localhost"),
-    'site_name' => env("COMMENT_SITE_NAME", "收藏屋"),
-    'site_count_url' => env("COMMENT_SITE_COUNT_URL", 'https://new.cnzz.com/v1/login.php?siteid=1278840518'),
+    'site_name' => env("COMMENT_SITE_NAME", "网站名称"),
+    'site_count_url' => env("COMMENT_SITE_COUNT_URL", ''),
     'enable_page_share' => env("COMMENT_ENABLE_PAGE_SHARE", false),
     'enable_page_analytics' => env("COMMENT_ENABLE_PAGE_ANALYTICS", false)
 ];
